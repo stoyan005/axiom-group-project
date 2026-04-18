fe754113db32dac40c9a233848cc785b95ad2779
Stoyan Ivanov <ivanov.stoyan100@gmail.com>
2026-04-18 22:38:40 +0100

    Created all the apps needed for the website.

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Organization(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=255)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, related_name="departments")
+    leader = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class TeamType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name="teams")
+    team_type = models.ForeignKey(TeamType, on_delete=models.SET_NULL, null=True)
+    specialization = models.CharField(max_length=255, blank=True)
+
+    dependencies = models.ManyToManyField("self", symmetrical=False, blank=True)
+
+    def __str__(self):
+        return self.name
