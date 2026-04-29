259f4944063b606a1e99be73d5c2db9570256303
almapar <alexutzamaria68@gmail.com>
2026-04-29 01:14:51 +0100

    - initiated the server - solved errors within running the server - created the database structure

---
+++ sky-engineering-portal-forTeamPage-main/planner/models.py
@@ -0,0 +1,20 @@
+# Author: Student 4
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    date = models.DateField()
+    
+    CATEGORY_CHOICES = [
+        ('maths', 'Maths'),
+        ('project', 'Project'),
+        ('personal', 'Personal'),
+    ]
+    
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='personal')
+
+    def __str__(self):
+        return self.title
